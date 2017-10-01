d6e641762970ab637954fec8beca990f0973c6a0
Gloria <gloria@Glorias-MacBook-Pro-2.local>
2017-10-01 18:44:22 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=id2980356_recipehashtag',
+    'username' => 'id2980356_admin',
+    'password' => '2017@groupproject',
+    'charset' => 'utf8',
+];
---
+++ models/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property string $authKey
+ * @property integer $active
+ */
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'active'], 'required'],
+            [['id', 'active'], 'integer'],
+            [['username', 'password'], 'string', 'max' => 30],
+            [['email'], 'string', 'max' => 40],
+            [['authKey'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'email' => 'Email',
+            'password' => 'Password',
+            'authKey' => 'Auth Key',
+            'active' => 'Active',
+        ];
+    }
+
+    public function getAuthKey(){
+        return $this->authKey;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function validateAuthKey($authKey){
+        return $this->authKey === $authKey;
+    }
+
+    public static function findIdentity($id){
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null){
+        throw new \yii\base\NotSupportedException();
+    }
+
+    public static function findByUsername($username){
+        return self::findOne(['username'=>$username]);
+    }
+
+    public function validatePassword($password){
+        return $this->password === $password;
+    }
+}
